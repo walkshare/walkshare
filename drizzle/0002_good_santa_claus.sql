c86c07605ca17499383cedfa18d63de4c1723736
matteopolak <matteopolak@hotmail.com>
2024-03-02 01:52:10 -0500

    fix: drizzle migration

--- drizzle/0002_good_santa_claus.sql
+++ drizzle/0002_good_santa_claus.sql
@@ -1,2 +1,3 @@
-ALTER TABLE "event" ADD COLUMN "embedding" "vector(768)" NOT NULL;--> statement-breakpoint
-ALTER TABLE "poi" ADD COLUMN "embedding" "vector(768)" NOT NULL;
+CREATE EXTENSION vector;--> statement-breakpoint
+ALTER TABLE "event" ADD COLUMN "embedding" vector(768) NOT NULL;--> statement-breakpoint
+ALTER TABLE "poi" ADD COLUMN "embedding" vector(768) NOT NULL;
