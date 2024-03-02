e8c0d8702e783a0eb2a06090162a0168eec19973
matteopolak <matteopolak@hotmail.com>
2024-03-02 01:54:32 -0500

    feat: only use vector if not exists already

--- drizzle/0002_good_santa_claus.sql
+++ drizzle/0002_good_santa_claus.sql
@@ -1,3 +1,3 @@
-CREATE EXTENSION vector;--> statement-breakpoint
+CREATE EXTENSION IF NOT EXISTS vector;--> statement-breakpoint
 ALTER TABLE "event" ADD COLUMN "embedding" vector(768) NOT NULL;--> statement-breakpoint
 ALTER TABLE "poi" ADD COLUMN "embedding" vector(768) NOT NULL;
