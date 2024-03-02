5e265580b02b83d1c73490b253959570f330ea0e
Vasil <vasiltopalovic@outlook.com>
2024-03-02 03:28:39 -0500

    feat: add friend routes

--- src/lib/server/routes/index.ts
+++ src/lib/server/routes/index.ts
@@ -1,9 +1,10 @@
 import { router } from '$lib/server/trpc';
 
 import event from './event';
+import friend from './friend'
 import poi from './poi';
 
-export const app = router({ event, poi });
+export const app = router({ event, poi, friend});
 
 export default app;
 export type Router = typeof app;
