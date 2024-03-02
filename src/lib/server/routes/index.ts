f3edaa209cf1050ecdd1da37947e174684532493
Vasil <vasiltopalovic@outlook.com>
2024-03-02 13:22:27 -0500

    feat: add itinerary route to routes

--- src/lib/server/routes/index.ts
+++ src/lib/server/routes/index.ts
@@ -2,12 +2,14 @@ import { router } from '$lib/server/trpc';
 
 import event from './event';
 import friend from './friend'
+import itinerary from './itinerary'
 import poi from './poi';
 
 export const app = router({
 	event,
 	poi,
 	friend,
+	itinerary,
 });
 
 export default app;
--- src/lib/server/routes/itinerary.ts
+++ src/lib/server/routes/itinerary.ts
@@ -5,8 +5,6 @@ import { procedure, router } from '$lib/server/trpc';
 import { createItinerary } from '../openai';
 import { Poi } from '../schema';
 
-
-
 export const app = router({
 	create: procedure.input(z.object({ lat: z.number().max(90).min(-90),
 		long: z.number().max(180).min(-180) })).output(Poi.array()).query(async ({input}) => {
