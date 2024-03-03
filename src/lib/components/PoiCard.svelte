66cdc24fdf3c1d3fd672c119c7832d9726623bdf
Vasil <vasiltopalovic@outlook.com>
2024-03-03 00:37:53 -0500

    feat: improve background on poicard

--- src/lib/components/PoiCard.svelte
+++ src/lib/components/PoiCard.svelte
@@ -8,7 +8,10 @@
 	export let background = false;
 </script>
 
-<div class="card w-full hover:bg-base-100 p-4 duration-200 transition-all" class:bg-base-200={background}>
+<div
+	class="card w-full hover:bg-neutral bg-base-100 p-4 duration-200 transition-all"
+	class:bg-base-200={background}
+>
 	<figure class="relative pt-0 mt-0">
 		<div class="w-full h-32 bg-base-300 rounded-xl overflow-hidden">
 			<img src="{PUBLIC_BASE_URL}/pois/{poi.id}/thumbnail" alt={poi.name} />
@@ -16,7 +19,9 @@
 
 		<div class="absolute top-2 left-2 flex flex-row flex-wrap gap-2">
 			{#each poi.tags.slice(0, 3) as tag}
-				<span class="badge badge-secondary bg-white border-white">{capitalize(tag)}</span>
+				<span class="badge badge-secondary bg-white border-white"
+					>{capitalize(tag)}</span
+				>
 			{/each}
 		</div>
 	</figure>
